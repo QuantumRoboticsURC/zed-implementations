0a5531ffd308c0dd02abb41cf84081e80bef1505
JOSE <joseangeldelangel10@gmail.com>
2023-01-25 02:07:16 -0600

    aruco experiment setup part 1

--- src/aruco_distance_experiments.py
+++ src/aruco_distance_experiments.py
@@ -1,29 +1,5 @@
 #!/usr/bin/env python3
 
-"""Made by:Erika García Sánchez
-	A01745158@tec.mx
-	erika.mgs@outlook.com
-	José Ángel del Ángel
-    joseangeldelangel10@gmail.com
-
-Modified (15/12/2022): 
-		José Ángel del Ángel and Erika García 16/12/2022 Aruco detection code cleanup 
-		José Ángel del Ángel 16/12/2022 Aruco mask with distance added
-
-Code description:
-TODO - update code description and notes 
-1. Ask the user for the name of a file.
-2. If the file exist at a predefined directory it opens it and read it.
-   If not, it creates a file with the name.
-3. Ask the user the action to execute (write lat & long | write the square).
-4. Close the file. 
-
-Notes:
-- Validate the user input
-- Add an exit option
-* Despite the code adds 0.00001 theorically, it's not exact.
-"""
-import rospy
 import pyzed.sl as sl
 import numpy as np
 import argparse
@@ -33,9 +9,8 @@ import sys
 import math
 from std_msgs.msg import String, Int8
 
-class ArucoDetector():
+class ExperimentHelper():
     def __init__(self, aruco_dict = cv2.aruco.DICT_4X4_50):
-        rospy.init_node("aruco_detector")
 
         # ________ aruco atributes initialization ______
         self.arucoDict = cv2.aruco.getPredefinedDictionary(aruco_dict)
@@ -56,8 +31,13 @@ class ArucoDetector():
         self.zed_runtime_parameters.confidence_threshold = 100
         self.zed_runtime_parameters.textureness_confidence_threshold = 100
         self.image_size = self.zed_camera.get_camera_information().camera_resolution
+
+        # TODO: IMPORTANT!!!! DEFINE THE IMAGE SIZE THAT WE WILL BE USING AT URC, SO THAT 
+        # VALUES IN EXPERIMENTS CORRESPOND TO THE VALUES AT COMPETITION
+        # =========================================================================
         self.image_size.width = self.image_size.width /2
         self.image_size.height = self.image_size.height /2
+        # =========================================================================
 
         self.image_zed = sl.Mat(self.image_size.width, self.image_size.height, sl.MAT_TYPE.U8_C4)
         self.depth_image_zed = sl.Mat(self.image_size.width, self.image_size.height, sl.MAT_TYPE.U8_C4)
@@ -69,10 +49,6 @@ class ArucoDetector():
         self.displayed_image_ocv = np.zeros((self.image_size.height, self.image_size.width, 3), dtype=np.uint8)
         self.arucos_mask = np.zeros((self.image_size.height, self.image_size.width, 3), dtype = np.uint8)
         self.arucos_mask_with_distance = np.zeros((self.image_size.height, self.image_size.width), dtype = np.float64)
-
-        # ________ ros atributes initialization ______
-        self.debug_topic = rospy.Publisher("/debug_print", String, queue_size=1)
-        self.aruco_distances_publisher = rospy.Publisher("/aruco_distances", String, queue_size = 1)
 
     def draw_arucos(sel, image, corners):
         # verify *at least* one ArUco marker was detected
@@ -107,7 +83,7 @@ class ArucoDetector():
     def midpoint_equation(self, p1, p2):
         return ( (p1[0]+p2[0])/2, (p1[1]+p2[1])/2 )
 
-    def get_aruco_midpoint(self, rectangle_corners):
+    def get_aruco_midpoint_and_area(self, rectangle_corners):
         """ function that returns the x,y,z cordinates of the aruco's midpoint """
         # ______________ initializing and formating data that will be used ______________
         self.arucos_mask = np.zeros((self.image_size.height, self.image_size.width, 3), dtype = np.uint8)
@@ -126,39 +102,22 @@ class ArucoDetector():
             z_center = (self.arucos_mask_with_distance/255.0).sum()/tag_area
         else:
             z_center = 0.0
-        return (float(x_center), float(y_center), float(z_center) )
-
-    def transform_aruco_midpoint_to_metric_system(aruco_midpoint):
-        pass
+        return (float(x_center), float(y_center), float(z_center), float(tag_area))
 
     def main(self):
-        while not rospy.is_shutdown():
-            self.arucos_mask = np.zeros((self.image_size.height, self.image_size.width, 3), dtype = np.int8)
-            self.arucos_mask_with_distance = np.zeros((self.image_size.height, self.image_size.width), dtype = np.float64)
-            if self.zed_camera.grab(self.zed_runtime_parameters) == sl.ERROR_CODE.SUCCESS:
-                # Retrieve left image
-                self.zed_camera.retrieve_image(self.image_zed, sl.VIEW.LEFT, sl.MEM.CPU, self.image_size)
-                # Retrieve depth map. Depth is aligned on the left image
-                self.zed_camera.retrieve_image(self.depth_image_zed, sl.VIEW.DEPTH, sl.MEM.CPU, self.image_size)
-                # Retrieve colored point cloud. Point cloud is aligned on the left image.
-                self.zed_camera.retrieve_measure(self.point_cloud, sl.MEASURE.DEPTH, sl.MEM.CPU, self.image_size)
-                self.image_ocv = self.image_zed.get_data()
-                self.image_ocv = self.image_ocv[:,:,:-1]
-                self.depth_image_ocv = self.depth_image_zed.get_data()
-                self.point_cloud_ocv = self.point_cloud.get_data()
-
-                aruco_corners, aruco_ids = self.get_arucos_info_in_image(self.image_ocv)
-                self.debug_topic.publish("aruco corners : {c}, aruco corners dtype {t}".format(c = aruco_corners, t = type(aruco_corners)))
-                self.displayed_image_ocv = self.image_ocv.copy()
-                self.displayed_image_ocv = self.draw_arucos(self.displayed_image_ocv, aruco_corners)                
-                aruco_centers = list(map(self.get_aruco_midpoint, aruco_corners))
-                self.debug_topic.publish("aruco_centers: {c}".format(c = aruco_centers))
-                cv2.imshow("image", self.displayed_image_ocv)
-                cv2.imshow("aruco mask", self.arucos_mask)
-                cv2.imshow("aruco mask with depth", np.uint8(self.arucos_mask_with_distance))
-                cv2.waitKey(1)                
-                # self.displayed_image_ocv = self.image_ocv.copy()
+        user_input = ""    
+        while True:
+            cap = cv2.VideoCapture(0)
+            _, frame = cap.read()
+            if user_input == "end":
+                break
+            elif user_input == "":
+                pass
+            else:
+            aruco_corners, aruco_ids = self.get_arucos_info_in_image(frame)
+            self.displayed_image_ocv = frame.copy()
+            aruco_centers_and_areas = list(map(self.get_aruco_midpoint, aruco_corners))
 
 if __name__ == "__main__":
-    aruco_detector = ArucoDetector()
+    aruco_detector = ExperimentHelper()
     aruco_detector.main()
